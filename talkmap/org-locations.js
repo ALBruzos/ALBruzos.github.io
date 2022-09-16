94ba48d50cd8c855a2e96c2f6c59df6d482da90c
Alicia L. Bruzos <albruzos@gmail.com>
2022-09-16 18:34:06 +0100

    update

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,27 +1,67 @@
 var addressPoints = [
-    [
-    "Cambridge, United Kingdom",
-    52.197584649999996,
-    0.13915373736874398
+  [
+    "O Grove, Spain\nCharla sobre mejillones",
+    42.4693872,
+    -8.8949654
   ],
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Vigo, Spain",
+    42.2376602,
+    -8.7247205
+  ],
+  [
+    "Salamanca, Spain",
+    40.9651572,
+    -5.6640182
+  ],
+  [
+    "Santiago de Compostela, Spain",
+    42.8804219,
+    -8.5458608
+  ],
+  [
+    "A Coru\u00f1a, Spain",
+    43.3709703,
+    -8.3959425
+  ],
+  [
+    "Cartagena, Spain",
+    37.6019353,
+    -0.9841152
+  ],
+  [
+    "Hinxton-Cambridge, UK",
+    52.0848693,
+    0.1792608
+  ],
+  [
+    "Madrid, Spain",
+    40.4167047,
+    -3.7035825
   ],
   [
     "London, UK",
     51.5073219,
-    -0.1276473
+    -0.1276474
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Torino, Italy",
+    45.0677551,
+    7.6824892
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Algarve, Portugal",
+    37.24480855,
+    -8.15076634627543
+  ],
+  [
+    "Porto, Portugal",
+    41.1494512,
+    -8.6107884
+  ],
+  [
+    "Berlin, Germany",
+    52.5170365,
+    13.3888599
   ]
 ];
