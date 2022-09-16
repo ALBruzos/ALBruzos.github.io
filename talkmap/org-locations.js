79df42ef9ced7843da475ede597e8e191a32be49
Alicia L. Bruzos <albruzos@gmail.com>
2022-09-16 19:02:15 +0100

    update

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,67 +1,97 @@
 var addressPoints = [
   [
-    "O Grove, Spain\nCharla sobre mejillones",
+    "O Grove, Spain:\n 2019 - XXII Foro dos Recursos Marinos e da Acuicultura das Rias Galegas;\n 2017 - XX Foro dos Recursos Marinos e da Acuicultura das Rias Galegas",
     42.4693872,
     -8.8949654
   ],
   [
-    "Vigo, Spain",
+    "Vigo, Spain: \n 2018 - VI International Symposium on Marine Sciences;\n 2018 - VI International Symposium on Marine Sciences;\n 2017 - I Annual Meeting CINBIO",
     42.2376602,
     -8.7247205
   ],
   [
-    "Salamanca, Spain",
-    40.9651572,
-    -5.6640182
-  ],
-  [
-    "Santiago de Compostela, Spain",
+    "Santiago de Compostela, Spain:\n 2021 - VIII Young Researchers Meeting;\n 2019 - II Annual CIMUS workshop;\n 2019 - VII Young Researchers Meeting;\n 2018 -VI Young Researchers Meeting-Health Science;\n 2018 - I Annual CIMUS workshop",
     42.8804219,
     -8.5458608
   ],
   [
-    "A Coru\u00f1a, Spain",
+    "A Coru\u00f1a, Spain: \n 2019 - VI Edicion Investigacion Biomedica del Cancer en Galicia (IBCG);\n 2018 - International Young Investigator Meeting (IYI);\n 2018 - X Seminario de Citogenetica de la SEG",
     43.3709703,
     -8.3959425
   ],
   [
-    "Cartagena, Spain",
+    "Salamanca, Spain:\n 2017 - 33 Encuentro de Jovenes Investigadores",
+    40.9651572,
+    -5.6640182
+  ],
+  [
+    "Cartagena, Spain: \n 2019 - XVII Congreso Nacional de Acuicultura",
     37.6019353,
     -0.9841152
   ],
   [
-    "Hinxton-Cambridge, UK",
-    52.0848693,
-    0.1792608
-  ],
-  [
-    "Madrid, Spain",
-    40.4167047,
-    -3.7035825
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276474
-  ],
-  [
-    "Torino, Italy",
-    45.0677551,
-    7.6824892
-  ],
-  [
-    "Algarve, Portugal",
+    "Algarve, Portugal:\n 2021 - Seminar at Centro de Ciencias do Mar (CCMAR)",
     37.24480855,
     -8.15076634627543
   ],
   [
-    "Porto, Portugal",
+    "Porto, Portugal:\n 2021 - CIBIO-InBIO Seminar Series;\n 2019 - 19th International Conference on Diseases of Fish and Shellfish",
     41.1494512,
     -8.6107884
   ],
   [
-    "Berlin, Germany",
+    "Lisboa, Portugal:\n 2022 - 19th Portugaliae Genetica",
+    39.21403,
+    -8.95766
+  ],
+  [
+    "Berlin, Germany:\n 2021 - Schwarz Lab Seminars at Max Delbruck Center for Molecular Medicine (virtual)",
     52.5170365,
     13.3888599
+  ],
+  [
+    "Kiel, Germany:\n 2022 - Ocean Health Symposium",
+    52.5170365,
+    13.3888599
+  ],
+  [
+    "Munich, Germany:\n 2022 - 21st ESPD Annual Meeting",
+    48.13513,
+    11.58198
+  ],
+  [
+    "Bannyuls sur mer, France:\n 2017 - Assemblee Generale EMBRC-France",
+    42.48343,
+    3.12887
+  ],
+  [
+    "Montpellier, France:\n 2022 - SEB Annual Conference",
+    43.61077,
+    3.87672
+  ],
+  [
+    "Hinxton-Cambridge, United Kingdom:\n 2019 - Evolution and Ecology of Cancer. Wellcome Genome Campus",
+    52.0848693,
+    0.1792608
+  ],
+  [
+    "Oxford, United Kingdom:\n 2022 - IX SRUK/CERU International Symposium",
+    51.75202,
+    -1.25773
+  ],
+  [
+    "London, UK:\n 2022 - Crick Cancer Research Symposium;\n 2021 - 5th Crick Autumn Science Meeting",
+    51.5073219,
+    -0.1276474
+  ],
+  [
+    "Torino, Italy:\n 2021 - EACR Congress. Innovative Cancer Science: Better Outcomes through Research",
+    45.0677551,
+    7.6824892
+  ],
+  [
+    "New Orleans, USA:\n 2022 - AACR Annual Meeting",
+    29.95107,
+    -90.07153
   ]
 ];
